ba47774e4971acc4cf36f7a380ff046b5117983b
Anushka Dilshan <87310590+AnushkaDilshan@users.noreply.github.com>
2023-05-21 18:00:55 +0530

    config2

--- Backend/server.js
+++ Backend/server.js
@@ -2,26 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
-const images = require('./middleware/imagepro');
+const images = require('./middleware/imageBlog');
+const projimages =require('./middleware/imagepro');
+const productimages =require('./middleware/imageProducts');
 
 const app = express();
 
 //import routes
-const postRoutes = require('./routes/volunteer');
-const postProject =  require('./routes/project');
-const postRegister =  require('./routes/register');
-
+const postRoutes = require('./routes/blog');
+const postRegister = require('./routes/register');
+const postVo = require('./routes/volunteer');
+const postProject= require('./routes/project');
+const postProducts= require('./routes/Products');
 //app middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 //route middleware
 app.use(postRoutes);
-app.use(postProject);
 app.use(postRegister);
+app.use(postVo);
+app.use(postProject);
+app.use(postProducts);
 
 const PORT = 8080;
-const DB_URL = 'mongodb+srv://Anushka:anushka@helpnature.td9rjdp.mongodb.net/?retryWrites=true&w=majority';
+const DB_URL = 'mongodb+srv://it21066016:vathila143@helpnature.td9rjdp.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(DB_URL,{
     useNewUrlParser:true,
@@ -33,13 +38,21 @@ connection.once("open",() =>{
     console.log("Mongodb connection success!")
 });
 
-app.use("/upload/project/:nic", images.single("project"), function (req, res) {
+app.use("/upload/blogPic/:nic", images.single("blogPic"), function (req, res) {
+res.json("Done");   
+});
+
+app.use("/upload/project/:nic", projimages.single("project"), function (req, res) {
     res.json("Done");   
     });
+
+    app.use("/upload/products/:nic", productimages.single("products"), function (req, res) {
+        res.json("Done");   
+        });
+
 
 
 app.listen(PORT, () =>{
     console.log(`App is running on ${PORT}`);
 });
-
 
