8adabc8b2ff83e5608e825ba2c090ac33a714f44
IT21060908 <IT21060908@my.sliit.lk>
2023-05-22 16:09:04 +0530

    login registration edit commit

--- frontend/src/components/auth/protected.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "../Login"
-function Protected  ({children}) {
-
-   const role = localStorage.getItem("userName");
-  return <div>{role ? children:<Login/>}</div>;
-
-};
-export default Protected;
