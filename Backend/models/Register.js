7a0e4ba14e7ef94be81924f88e105cb8f3f5d2e9
Vathila Rajapakse <87406802+VathilaRajapakse@users.noreply.github.com>
2023-05-23 13:06:11 +0530

    register commit

--- Backend/models/Register.js
+++ Backend/models/Register.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postRegister = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
 
-    fullname:{
-        type:String,
-        required:true
-    },
-    address:{
-        type:String,
-        required:true
-    },
-    contact:{
-        type:String,
-        required:true
-    },
+  email: {
+    type: String,
+    required: true,
+  },
 
-    email:{
-        type:String,
-        required:true
-    },
-    
-    userName:{
-        type:String,
-        required:true
-    },
+  userName: {
+    type: String,
+    required: true,
+  },
 
-    password:{
-        type:String,
-        required:true
-    },
-
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('register',postRegister);
+module.exports = mongoose.model("register", postRegister);
