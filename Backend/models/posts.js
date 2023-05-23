7d5256d300e85d7b02c7ad4008f9a5111ba5a5c2
Vathila Rajapakse <87406802+VathilaRajapakse@users.noreply.github.com>
2023-05-23 13:05:37 +0530

    backend commit

--- Backend/models/posts.js
+++ Backend/models/posts.js
@@ -1,27 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    blogger_name:{
-        type:String,
-        required:true
-    },
+  blogger_name: {
+    type: String,
+    required: true,
+  },
 
-    blog_title:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    // image:{
-    //     type:String,
-    //     required:true
-    // },
-    
-
+  blog_title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  // image:{
+  //     type:String,
+  //     required:true
+  // },
 });
 
-
-
-module.exports = mongoose.model('blogs',postSchema);
+module.exports = mongoose.model("blogs", postSchema);
